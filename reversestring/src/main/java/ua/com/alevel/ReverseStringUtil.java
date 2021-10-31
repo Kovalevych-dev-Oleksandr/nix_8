e6b63ac29efcf66f6a2a5547d9a99bad4297ac25
Kovalevych-dev-Oleksandr <w64026@student.wsiz.edu.pl>
2021-10-31 22:23:31 +0100

    hw_2_strings_library

--- reversestring/src/main/java/ua/com/alevel/ReverseStringUtil.java
+++ reversestring/src/main/java/ua/com/alevel/ReverseStringUtil.java
@@ -2,9 +2,6 @@ package ua.com.alevel;
 
 public final class ReverseStringUtil {
 
-    private ReverseStringUtil() {
-    }
-
     public static String reverseString(String usersString) {
 
         char[] array = usersString.toCharArray();
@@ -29,16 +26,18 @@ public final class ReverseStringUtil {
     public static String reverseSpecifiedWordInLine(String usersString, int indexArray) {
         String reverseUsersString = "";
         String[] array = usersString.split("\\s+");
-        if (indexArray<= array.length && indexArray>=1){
+        if (indexArray <= array.length && indexArray >= 1) {
             for (int i = 0; i < array.length; i++) {
-             if (i == indexArray) {
-                   reverseUsersString = reverseUsersString + reverseString(array[i]);
-             } else {
-                  reverseUsersString = reverseUsersString + array[i] + " ";
-             }
-             }
+                if (i == indexArray) {
+                    reverseUsersString = reverseUsersString + reverseString(array[i]);
+                } else {
+                    reverseUsersString = reverseUsersString + array[i] + " ";
+                }
+            }
 
-            }else{return "An existing word number has been entered";}
+        } else {
+            return "An existing word number has been entered";
+        }
 
         return reverseUsersString;
     }
@@ -46,7 +45,7 @@ public final class ReverseStringUtil {
     public static String reverseSpecifiedLetterInString(String usersString, int indexLetterArrayFirst, int indexLetterArrayLast) {
         String reverseUsersString = "";
         char[] array = usersString.toCharArray();
-         if(сheckingTheIndex(indexLetterArrayFirst,array.length)==true && сheckingTheIndex(indexLetterArrayLast,array.length)==true ){
+        if (сheckingTheIndex(indexLetterArrayFirst, array.length) == true && сheckingTheIndex(indexLetterArrayLast, array.length) == true) {
             for (int i = 0; i < indexLetterArrayFirst; i++) {
                 reverseUsersString = reverseUsersString + array[i];
             }
@@ -56,18 +55,21 @@ public final class ReverseStringUtil {
             for (int i = indexLetterArrayLast; i < array.length; i++) {
                 reverseUsersString = reverseUsersString + array[i];
             }
-        }else{return " The entered element numbers do not match the existing ones in the array";}
+        } else {
+            return " The entered element numbers do not match the existing ones in the array";
+        }
 
         return reverseUsersString + " ";
 
     }
+
     //if(indexLetterArrayFirst<=array.length && indexLetterArrayLast<=array.length && indexLetterArrayLast>=0 && indexLetterArrayFirst=>0)
-    public static boolean сheckingTheIndex (int index, int size){
-        boolean result;
-        if (index<=size && index>=0) {
+    public static boolean сheckingTheIndex(int index, int size) {
+        final boolean result;
+        if (index <= size && index >= 0) {
             result = true;
-        }else {
-            result =false;
+        } else {
+            result = false;
         }
         return result;
     }
