b96a7b7e8c3856d309ce2a5b8f665d05035b92a2
Kovalevych-dev-Oleksandr <w64026@student.wsiz.edu.pl>
2021-11-02 23:27:01 +0100

    hw_2_strings_library

---
+++ module_1/src/main/java/ua/com/alevel/ModuleOneMain.java
@@ -0,0 +1,12 @@
+package ua.com.alevel;
+
+import ua.com.alevel.firstlevel.ProgramLevelOneRun;
+
+public class ModuleOneMain {
+
+    public static void main(String[] args){
+        ProgramLevelOneRun.run();
+
+    }
+
+}
---
+++ module_1/src/main/java/ua/com/alevel/firstlevel/ProgramLevelOneRun.java
@@ -0,0 +1,60 @@
+package ua.com.alevel.firstlevel;
+
+import ua.com.alevel.EventProcessor;
+import ua.com.alevel.uniquecharactersinthearray.UniqueCharactersInTheArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class ProgramLevelOneRun {
+
+    public static void run() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        ProgramLevelOneRun.print();
+        String event;
+        try {
+            while ((event = reader.readLine()) != null) {
+                EventProcessor eventProcessor = getEvent(event);
+                if (eventProcessor != null) {
+                    eventProcessor.process(reader, out);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static EventProcessor getEvent(String event) {
+        if ("1".equals(event)) {
+             return new UniqueCharactersInTheArray();
+        }
+
+        if ("2".equals(event)) {
+            //return new FlipAccordingToTheWordOrder();
+        }
+        if ("3".equals(event)) {
+            //return new ReverseOfTheSpecifiedWordInTheLine();
+        }
+        if ("4".equals(event)) {
+            // return new ReverseSpecifiedLetter();
+        }
+
+        if ("5".equals(event)) {
+            //exit(0);
+        }
+        return null;
+    }
+
+    private static void print() {
+        out.println("If you need Unique Characters In The Array, choose 1");
+        for (String s : Arrays.asList("If you need a reverse by words, choose 2 ", "If you need a choose a word to reverse, choose 3 ", "If you need areverse of letters in a lineselect 4 ", "If you want to finish, select 5", "Select you event: ")) {
+            out.println(s);
+        }
+    }
+}
