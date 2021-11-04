faa241791fe38b58ad7c51193569bb2b595da39c
Kovalevych-dev-Oleksandr <w64026@student.wsiz.edu.pl>
2021-11-04 20:43:45 +0100

    module_1 task_3

--- module_1/src/main/java/ua/com/alevel/horsemove/HorseMove.java
+++ module_1/src/main/java/ua/com/alevel/horsemove/HorseMove.java
@@ -23,9 +23,9 @@ public class HorseMove implements EventProcessor {
         writer.println("The size of your chessboard is 8X8, columns and rows are numbered from 1 to 8, please enter positive numbers from one to eight");
         writer.println("Select a starting position");
         writer.println("Enter a Cols:");
-        userStartPositionInRows = str.nextLine();
-        writer.println("Enter a Rows:");
         userStartPositionInCols = str.nextLine();
+        writer.println("Enter a Rows:");
+        userStartPositionInRows = str.nextLine();
 
         if (UniqueCharactersInTheArray.checkingTheString(userStartPositionInCols) == true && UniqueCharactersInTheArray.checkingTheString(userStartPositionInRows) == true) {
             printChess(Integer.parseInt(userStartPositionInCols), Integer.parseInt(userStartPositionInRows));
@@ -38,15 +38,13 @@ public class HorseMove implements EventProcessor {
 
         writer.println("Select a Next position");
         writer.println("Enter a Cols:");
-        userNextPositionInRows = str.nextLine();
-        writer.println("Enter a Rows:");
         userNextPositionInCols = str.nextLine();
+        writer.println("Enter a Rows:");
+        userNextPositionInRows = str.nextLine();
 
-        if (UniqueCharactersInTheArray.checkingTheString(userNextPositionInCols) == false && UniqueCharactersInTheArray.checkingTheString(userNextPositionInRows) == false) {
-            writer.println("Next time enter a number from one to eight in a string ");
-            printEnd();
-            return;
-        } else {
+
+        if (UniqueCharactersInTheArray.checkingTheString(userNextPositionInCols) == true && UniqueCharactersInTheArray.checkingTheString(userNextPositionInRows) == true) {
+
             if (checkingTheHorseCourse(Integer.parseInt(userStartPositionInCols), Integer.parseInt(userNextPositionInCols), Integer.parseInt(userStartPositionInRows), Integer.parseInt(userNextPositionInRows)) == true) {
 
                 writer.println("Step is possible");
@@ -58,12 +56,16 @@ public class HorseMove implements EventProcessor {
                 return;
             }
             printEnd();
+        } else {
+            writer.println("Next time enter a number from one to eight in a string ");
+            printEnd();
+            return;
 
         }
     }
 
 
-    public static void printChess(int userNextPositionInCols, int userNextPositionInRols) {
+    public static void printChess(int userNextPositionInRows,int userNextPositionInCols) {
         for (int i = 0; i < SIZE_ROLS_AND_COLS + 1; i++) {
             for (int j = 0; j < SIZE_ROLS_AND_COLS + 1; j++) {
                 if (i == 0 && j == 0) {
@@ -72,7 +74,7 @@ public class HorseMove implements EventProcessor {
                     System.out.print(" " + (j) + " ");
                 } else if (j == 0) {
                     System.out.print(i);
-                } else if (i == userNextPositionInCols && j == userNextPositionInRols) {
+                } else if (i == userNextPositionInCols && j == userNextPositionInRows) {
                     System.out.print("!2!");
 
                 } else {
@@ -83,20 +85,20 @@ public class HorseMove implements EventProcessor {
         }
     }
 
-    public static boolean checkingTheHorseCourse(int userStartPositionInCols, int userNextPositionInCols, int userStartPositionInRols, int userNextPositionInRols) {
-        int CheckingTheDifferenceCols = userNextPositionInCols - userStartPositionInCols;
-        int CheckingTheDifferenceRols = userNextPositionInRols - userStartPositionInRols;
+    public static boolean checkingTheHorseCourse(int userStartPositionInCols, int userNextPositionInCols, int userStartPositionInRows, int userNextPositionInRows) {
+        int CheckingTheDifferenceCols = Math.abs(userNextPositionInCols - userStartPositionInCols);
+        int CheckingTheDifferenceRols = Math.abs(userNextPositionInRows - userStartPositionInRows);
         boolean result = true;
         result = (CheckingTheDifferenceCols == 1 && CheckingTheDifferenceRols == 2) || (CheckingTheDifferenceCols == 2 && CheckingTheDifferenceRols == 1);
         return result;
     }
 
-    public static boolean TestuserString(String userLine) {
+  /*  public static boolean TestuserString(String userLine) {
 
         final boolean result = UniqueCharactersInTheArray.checkingTheString(userLine) == true;
 
         return result;
-    }
+    }*/
 
     public static void printEnd() {
         System.out.println();
