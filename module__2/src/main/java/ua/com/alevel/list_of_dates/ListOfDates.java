fb593acea3f2697246e0ae833271eb019fb21472
Kovalevych-dev-Oleksandr <w64026@student.wsiz.edu.pl>
2021-12-30 00:17:02 +0100

    process commit task 1-OK

--- module__2/src/main/java/ua/com/alevel/list_of_dates/ListOfDates.java
+++ module__2/src/main/java/ua/com/alevel/list_of_dates/ListOfDates.java
@@ -4,66 +4,85 @@ import java.io.IOException;
 import java.util.List;
 
 import static ua.com.alevel.utils.Utils.readFile;
+import static ua.com.alevel.utils.Utils.writeInFile;
 
 public final class ListOfDates {
-    private static final String FILE_NAME = "list_of_dates.txt";
-    private static final int LENGTH_OF_THE_ENTRY_IN_THE_LINE = 13;
+    private static final String READ_FILE_NAME = "input_list_of_dates.txt";
+    private static final String WRITE_FILE_NAME = "output_list_of_dates.txt";
+    private static final int LENGTH_OF_THE_ENTRY_IN_THE_LINE = 1;
     private static final int SIZE_YEAR = 4;
     private static final int COUNT_MONTH = 12;
+    private static final int DAY_IN_MONTH = 31;
 
 
-    public static void run() throws IOException {
-        List<String> arrayList = (readFile(ListOfDates.FILE_NAME));
+    public static List<String> run() throws IOException {
+        List<String> arrayList = (readFile(ListOfDates.READ_FILE_NAME));
         String bufferLine;
         for (int i = 0; i < arrayList.size(); i++) {
             bufferLine = arrayList.get(i);
-            if (ListOfDates.LENGTH_OF_THE_ENTRY_IN_THE_LINE > bufferLine.length()) {
-                bufferLine = removeCharAt(bufferLine);
+            if (bufferLine.isBlank() && ListOfDates.LENGTH_OF_THE_ENTRY_IN_THE_LINE < bufferLine.length()) {
+                arrayList.remove(i);
+                continue;
+            } else {
                 bufferLine = stringTransformation(bufferLine);
                 arrayList.set(i, bufferLine);
             }
+
         }
-        System.out.println(arrayList);
+        writeInFile(WRITE_FILE_NAME, arrayList);
+        return arrayList;
+
     }
 
     private static String removeCharAt(String line) {
-        String clearLine = "";
-
-        for (int i = 1; i < line.length() - 1; i++) {
-            clearLine += line.charAt(i);
-        }
-
-        return clearLine;
+        StringBuilder clearLine = new StringBuilder();
+        clearLine.append(line);
+        System.out.println(line);
+        clearLine.deleteCharAt(0);
+        clearLine.deleteCharAt(clearLine.length() - 1);
+        return clearLine.toString();
     }
 
     private static String stringTransformation(String line) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
+        String userExceptionsOne = "-> Incorrect input format in this date. Example type “2020/04/05”or{2020/04/0}or|2020/04/05| or{2020/04/0}or[2020/04/05] or '2020/04/05'";
+        String userExceptionsTwo = "->  Day or month more possible values";
         try {
             String[] array;
-            array = line.split("/");
+            array = removeCharAt(line).split("/");
             int elementOfArrayYear = 0;
-            int elementOfArrayDay = 0;
             int elementOfArrayMonth = 0;
             for (int i = 0; i < array.length; i++) {
                 if (array[i].length() == SIZE_YEAR) {
                     elementOfArrayYear = i;
                 }
-                if (array[i].length() < SIZE_YEAR && Integer.parseInt(array[i]) <= COUNT_MONTH) {
+                if (ListOfDates.SIZE_YEAR > array[i].length() && ListOfDates.COUNT_MONTH >= Integer.parseInt(array[i])) {
                     elementOfArrayMonth = i;
                 }
 
             }
-            elementOfArrayDay = array.length - (elementOfArrayMonth + elementOfArrayYear);
-            for (int i : new int[]{elementOfArrayYear, elementOfArrayMonth, elementOfArrayDay}) {
-                result += array[i];
-            }
-            if (result.length() != 8) {
-                result = "not true";
-            }
-        } catch (Exception exception) {
-            result = line + "-> incorrect input format in this date. Example “2020/04/05” or{2020/04/0}or[2020/04/05] or '2020/04/05'";
-        }
-        return result;
+            int elementOfArrayDay = array.length - (elementOfArrayMonth + elementOfArrayYear);
+            if (ListOfDates.DAY_IN_MONTH >= Integer.parseInt(array[elementOfArrayDay]) && COUNT_MONTH >= Integer.parseInt(array[elementOfArrayMonth])) {
+                for (int i : new int[]{elementOfArrayYear, elementOfArrayMonth, elementOfArrayDay}) {
+                    result.append(array[i]);
+                }
+            } else if (result.toString().matches("(?i).*[a-zа-я].*") || result.toString().matches("[a-zA-Zа-яА-Я]+")) {
+                resultUserException(line, result, userExceptionsOne);
 
+            } else {
+                resultUserException(line, result, userExceptionsTwo);
+            }
+
+        } catch (Exception exception) {
+            resultUserException(line, result, userExceptionsOne);
+        }
+        return result.toString();
+
+    }
+
+    private static void resultUserException(String line, StringBuilder result, String userExceptions) {
+        result.delete(0, result.length());
+        result.append(line);
+        result.append(userExceptions);
     }
 }
--- module__2/src/main/java/ua/com/alevel/utils/Utils.java
+++ module__2/src/main/java/ua/com/alevel/utils/Utils.java
@@ -1,8 +1,8 @@
 package ua.com.alevel.utils;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,12 +14,26 @@ public class Utils {
 
         BufferedReader reader = new BufferedReader(new FileReader(fileName));
         String line;
-        while (null != (line = reader.readLine())) {
+        while ((line=reader.readLine())!=null) {
             arrayList.add(line);
         }
         reader.close();
-
-
         return arrayList;
+    }
+
+
+    public static String writeInFile(String fileName, List<String> arrayList) {
+        try {
+            FileWriter writer = new FileWriter(fileName, false);
+            for (int i = 0; i < arrayList.size(); i++) {
+                writer.write(arrayList.get(i));
+                writer.append('\n');
+            }
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "file not write";
+        }
+        return "file write";
     }
 }
